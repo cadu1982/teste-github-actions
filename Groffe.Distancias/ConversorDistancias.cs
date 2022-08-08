bcd1ae4827f02fb6b1ff10976e9392cc37b6e572
CADU1982 <cadu.lp@gmail.com>
2022-08-08 13:29:59 -0300

    fix: teste9

--- Groffe.Distancias/ConversorDistancias.cs
+++ Groffe.Distancias/ConversorDistancias.cs
@@ -12,9 +12,4 @@ namespace Groffe.Distancias
             km / 1.609; // Simulação de falha
             //Math.Round(km / 1.609, 3);
     }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> 05967a9b37aa28fa7539d444ebaf019548ece7a7
